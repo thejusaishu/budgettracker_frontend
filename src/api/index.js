15e06ca077c0a3557ee3ad634539398f9bac2a44
thejusaihu <thejusaishu@gmail.com>
2026-02-12 04:17:42 +0530

    Fixed API URL

--- src/api/index.js
+++ src/api/index.js
@@ -1,4 +1,4 @@
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000/api';
+const API_URL = 'https://budget-tracker-ygmt.onrender.com/api'  || 'http://localhost:3000/api';
 
 // Get token from localStorage
 function getToken() {
